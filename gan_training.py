a092de966a98f7b208382ec820beccc0d14f892a
Gerardo Salazar <Drifta10.gs@gmail.com>
2020-08-19 16:10:50 -0500

    Wrote training scripts

--- gan_training.py
+++ gan_training.py
@@ -1,2 +1,27 @@
 from models import GAN, train_gan
-from data import fetch_data
+from data import fetch_data, transform_for_gan
+from utils import plot_loss, sample_from_gan
+import argparse
+
+#Command line argument parsing
+parser = argparse.ArgumentParser(description='Specify model hyperparameters')
+parser.add_argument('--coding_size', type=int, help="Specify integer size of latent codings")
+parser.add_argument('--epochs', type=int, help="Specify integer number of epochs")
+args = parser.parse_args()
+
+#Get data
+batch_size = 32
+raw_data = fetch_data()
+data = transform_for_gan(raw_data, batch_size=batch_size)
+
+#Build model
+gan = GAN(coding_size=args.coding_size)
+
+#Train model
+train_gan(gan, data, batch_size, args.coding_size, args.epochs)
+
+#Plot samples
+sample_from_gan(gan, n_samples=5)
+
+#Save model
+gan.save('Models/GAN')
--- vae_training.py
+++ vae_training.py
@@ -2,24 +2,33 @@ from models import VAE
 from data import fetch_data
 from losses import vae_loss
 from utils import plot_loss, sample_from_vae
+import argparse
 
-#TODO: Add command line argument parsing
+#Command line argument parsing
+parser = argparse.ArgumentParser(description='Specify model hyperparameters')
+parser.add_argument('--conv_layers', type=int, help="Specify integer number of convolutional layers")
+parser.add_argument('--latent_dims', type=int, help="Specify integer number of learned latent dimensions")
+parser.add_argument('--epochs', type=int, help="Specify integer number of epochs")
+args = parser.parse_args()
 
 #Get data
-batch_size=32
+batch_size = 32
 raw_data = fetch_data()
 data = transform_for_vae(raw_data, batch_size=batch_size)
 
 #Build model
-vae = VAE(conv_layers=4, latent_dims=2)
+vae = VAE(args.conv_layers, args.latent_dims)
 vae.add_loss(vae_latent_loss)
 vae.compile(loss=vae_loss, optimizer="adam")
 
 #Train model
-history = vae.fit(data, data, epochs=50, batch_size=32)
+history = vae.fit(data, data, epochs=args.epochs, batch_size=32)
 
 #Plot diagnostics
 plot_loss(history)
 
 #Plot samples
 sample_from_vae(vae, n_samples=5)
+
+#Save model
+vae.save('Models/VAE')
