673495115e435c7d5a9c9f24df779c7ceb9ff6dd
Gerardo Salazar <Drifta10.gs@gmail.com>
2020-09-29 22:06:07 -0500

    Updated configuration script, succesfully tested

--- configure_aws.sh
+++ configure_aws.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 #Load .env file containing AWS credentials
 set -a
 source .env
 set +a
 
-#Make directory for AWS credentials in default location
-mkdir ~/.aws/
+#Make directory for AWS credentials in default location if it does not already exist
+if [[ ! -d ~/.aws/ ]]
+then
+    mkdir ~/.aws/
+fi
 
 #Switch to AWS directory and create credentials file
 cd ~/.aws/
@@ -12,9 +17,10 @@ touch credentials
 
 #Write AWS credentials to appropriate file
 filepath=~/.aws/credentials
-if [ -f "$filepath" ]
+if [[ -f "$filepath" ]]
 then 
-    echo "[default]"
-    echo "aws_access_key_id = $ACCESS_KEY_ID" > "$filepath"
-    echo "aws_secret_access_key = $SECRET_ACCESS_KEY" > "$filepath"
+    > "$filepath"
+    echo "[default]" >> "$filepath"
+    echo "aws_access_key_id = $ACCESS_KEY_ID" >> "$filepath"
+    echo "aws_secret_access_key = $SECRET_ACCESS_KEY" >> "$filepath"
 fi
